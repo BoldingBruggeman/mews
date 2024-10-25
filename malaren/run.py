2eacaf38c5807130cf38cfb3414eb432b97abbd3
Karsten Bolding <karsten@bolding-bruggeman.com>
2024-10-25 11:31:06 +0200

    updated run.py to be platform independent

--- malaren/run.py
+++ malaren/run.py
@@ -5,33 +5,41 @@ import calendar
 from pathlib import Path
 import subprocess
 
-start_date = date(2022, 1, 1)
-# stop_date = date(2022, 4, 1)
-stop_date = date(2023, 1, 1)
-
 setup = "malaren"
 script = "run_malaren.py"
 np = 4
 
-start = date(2022, 4, 1)
+start_date = date(2022, 1, 1)
+stop_date = date(2023, 1, 1)
+
+#start = date(2022, 4, 1)
 start = start_date
+
 while start < stop_date:
+    command = [
+        "mpiexec",
+        "-n",
+        str(np),
+        "python",
+        script,
+    ]
+
     days_in_month = calendar.monthrange(start.year, start.month)[1]
     stop = start + timedelta(days=days_in_month)
     x = start.strftime("%Y-%m-%d %H:%M:%S")
     y = stop.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Simulation from {x} to {y}")
 
+    command.extend([x, y])
+
     x = start.strftime("%Y%m%d")
     y = stop.strftime("%Y%m%d")
     if start == start_date:
-        option = "--initial"
-        restart_in = ""
+        command.extend(["--initial"])
     else:
-        option = ""
-        restart_in = f"--load_restart restart_{setup}_{x}.nc"
+        command.extend([f"--load_restart", f"restart_{setup}_{x}.nc"])
 
-    restart_out = f"--save_restart restart_{setup}_{y}.nc"
+    command.extend([f"--save_restart", f"restart_{setup}_{y}.nc"])
 
     p = Path(x)
     if p.is_dir():
@@ -39,19 +47,12 @@ while start < stop_date:
         exit()
     else:
         p.mkdir(parents=True)
-    output_dir = f"--output_dir {x}"
 
-    command = 'mpiexec -np %d python %s "%s" "%s" %s %s %s %s' % (
-        np,
-        script,
-        start.strftime("%Y-%m-%d %H:%M:%S"),
-        stop.strftime("%Y-%m-%d %H:%M:%S"),
-        option,
-        restart_in,
-        restart_out,
-        output_dir,
-    )
-    subprocess.run([command], shell=True)
-    # subprocess.run(["mv", "getm-\*.log", x])
+    command.extend(["--output_dir", "x"])
+
+    if True:
+        print(command)
+    else:
+        subprocess.run(command)
 
     start = stop
